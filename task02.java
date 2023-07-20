c0029b213c507b5d70ef259f6e9c91a7c08f3607
Tomo4kap <tomo4kap@gmail.ru>
2023-07-20 12:48:29 +0300

    Java_Lasson_1_task_2

---
+++ task02.java
@@ -0,0 +1,17 @@
+// Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
+
+public class task02 {
+static void Task02(){
+        int[] arr = {1,1,0,1,1,1};
+        int max = 0;
+        int count = 0;
+            for (int i=0; i < arr.length; i++){
+                if (arr[i] == 1) count++;
+                if (arr[i] == 0) {
+                    if (count > max) max = count;
+                    count = 0;
+                }
+            }
+        System.out.println(count);
+    }
+}
